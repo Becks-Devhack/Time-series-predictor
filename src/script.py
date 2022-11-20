c06ba0eb8bb6eea5bc5e94913bd0eefd3823887a
PerhapsS44 <stefan.saraev@yahoo.com>
2022-11-20 03:52:27 +0200

    added vacantion forecasting

--- src/script.py
+++ src/script.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from flask import request, jsonify, Response, Flask
 import pandas as pd 
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 
 stress_classifier = tf.keras.models.load_model('models/stress_classifier')
 app = Flask(__name__)
@@ -9,7 +10,6 @@ app = Flask(__name__)
 def get_stress_level():
     global stress_classifier
     payload = request.get_json(silent=True)
-    print(request)
     if not payload:
         # Error handling
         return Response(status=400)
@@ -29,8 +29,26 @@ def get_stress_level():
 
 @app.route("/vacantion_prediction", methods=["GET"])
 def get_vacantion_prediction():
-    global stress_classifier
-    return jsonify({'prediction':False}), 200
+    payload = request.get_json(silent=True)
+    if not payload:
+        # Error handling
+        return Response(status=400)
+    stress = payload["time_series"]
+    data = {"Stress":stress}
+    parsed_data = pd.DataFrame(data, dtype=float)
+
+    SARIMAXmodel = SARIMAX(parsed_data["Stress"], order = (1, 0, 1))
+    SARIMAXmodel = SARIMAXmodel.fit()
+
+    y_pred = SARIMAXmodel.get_forecast(10) # Magic number
+    y_pred_df = y_pred.conf_int(alpha = 0.05) 
+    y_pred_df["Predictions"] = SARIMAXmodel.predict(start = y_pred_df.index[0], end = y_pred_df.index[-1])
+    
+    vacantion = False
+    if y_pred_df["Predictions"].values[-1] - y_pred_df["Predictions"].values[0] > 0.5:
+        vacantion = True
+    
+    return jsonify({'prediction':vacantion}), 200
 
 if __name__ == '__main__':
     print(stress_classifier.summary())
